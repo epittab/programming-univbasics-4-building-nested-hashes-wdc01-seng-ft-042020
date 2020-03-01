a568425c59472ec1df19d851124afcdc4bd640ec
Enrique Pittaluga <ep7m@me.com>
2020-03-01 01:26:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
     :pieces => 4,
     :rent_in_dollars => {
       one_piece_owned: 25,
-      tier2: '',
+      two_pieces_owned: '',
       tier3: '',
       tier4: ''
     },
