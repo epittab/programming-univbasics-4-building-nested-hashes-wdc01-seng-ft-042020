7330fc013788921d91ae3b09e34ad3930db062ed
Enrique Pittaluga <ep7m@me.com>
2020-03-01 01:21:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,12 @@ def monopoly_with_third_tier
   # complexity.
   {railroads: {
     :pieces => 4,
-    :rent_in_dollars => [{}, {}, {}, {}]
+    :rent_in_dollars => {
+      tier1: '',
+      tier2: '',
+      tier3: '',
+      tier4: ''
+    }
   }}
 end
 
